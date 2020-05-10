981526984b8805fc60eb3c2447ec2b58a65ed526
Jenny Hong <jenny@jennys-mbp.lan>
2020-05-10 14:55:28 +0900

    add suggestions

--- graphql/db.js
+++ graphql/db.js
@@ -1,58 +1,88 @@
-let movies = [
-  {
-    id: 0,
-    name: "The Sound of Music",
-    score: 10,
-  },
-  {
-    id: 1,
-    name: "The Notebook",
-    score: 9,
-  },
-  {
-    id: 2,
-    name: "Ford v Ferrari",
-    score: 8,
-  },
-];
+// let movies = [
+//   {
+//     id: 0,
+//     name: "The Sound of Music",
+//     score: 10,
+//   },
+//   {
+//     id: 1,
+//     name: "The Notebook",
+//     score: 9,
+//   },
+//   {
+//     id: 2,
+//     name: "Ford v Ferrari",
+//     score: 8,
+//   },
+// ];
 
-export const getMovies = () => movies;
+// export const getMovies = () => movies;
 
-export const getById = (id) => {
-  const filteredMovies = movies.filter((movie) => movie.id === id);
-  return filteredMovies[0];
-};
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else return false;
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: movies.length,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
-};
-
-// import fetch from "node-fetch";
-// const API_URL = "https://yts.mx/api/v2/list_movies.json?";
-
-// export const getMovies = (limit, rating) => {
-//   let REQUEST_URL = API_URL;
-//   if (limit > 0) {
-//     REQUEST_URL += `limit=${limit}`;
-//   }
-//   if (rating > 0) {
-//     REQUEST_URL += `&minimum_rating=${rating}`;
-//   }
-//   return fetch(`${REQUEST_URL}`)
-//     .then((res) => res.json())
-//     .then((json) => json.data.movies);
+// export const getById = (id) => {
+//   const filteredMovies = movies.filter((movie) => movie.id === id);
+//   return filteredMovies[0];
 // };
+
+// export const deleteMovie = (id) => {
+//   const cleanedMovies = movies.filter((movie) => movie.id !== id);
+//   if (movies.length > cleanedMovies.length) {
+//     movies = cleanedMovies;
+//     return true;
+//   } else return false;
+// };
+
+// export const addMovie = (name, score) => {
+//   const newMovie = {
+//     id: movies.length,
+//     name,
+//     score,
+//   };
+//   movies.push(newMovie);
+//   return newMovie;
+// };
+
+import axios from "axios";
+const BASE_URL = "https://yts.mx/api/v2/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
+
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating,
+    },
+  });
+  return movies;
+};
+
+export const getMovie = async (id) => {
+  const {
+    data: {
+      data: { movie },
+    },
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+  return movie;
+};
+
+export const getSuggestions = async (id) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+  return movies;
+};
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,22 +1,24 @@
-import { getMovies, getById, addMovie, deleteMovie } from "./db";
-
-const resolvers = {
-  Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => getById(id),
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, { id }) => deleteMovie(id),
-  },
-};
-
-// import { getMovies } from "./db";
+// import { getMovies, getById, addMovie, deleteMovie } from "./db";
 
 // const resolvers = {
 //   Query: {
-//     movies: (_, { limit, rating }) => getMovies(limit, rating),
+//     movies: () => getMovies(),
+//     movie: (_, { id }) => getById(id),
+//   },
+//   Mutation: {
+//     addMovie: (_, { name, score }) => addMovie(name, score),
+//     deleteMovie: (_, { id }) => deleteMovie(id),
 //   },
 // };
+
+import { getMovies, getMovie, getSuggestions } from "./db";
+
+const resolvers = {
+  Query: {
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id),
+  },
+};
 
 export default resolvers;
